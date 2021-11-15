244fb0251e43d7ebf45193fc909f4aa3f3096295
zongii <zongi2000zongi@gmail.com>
2021-11-16 00:22:05 +0100

    Changed Website

--- script.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = document.getElementsByClassName("toggle-button")[0]
-const navbarlinks = document.getElementsByClassName("navbar-links")[0]
-
-togglebutton.addEventListener("click", () => {
-    navbarlinks.classList.toggle('active')
-})
