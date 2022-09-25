5d6b8bac2df0267f16faab6b5f291c8389699c14
Zongi <67127030+zongii@users.noreply.github.com>
2022-09-26 00:33:51 +0200

    Deployed new portfolio

--- js/smooth-anchor.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', 'a[href^="#"]', function(event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top - $('#nav-container').height()
-    }, 500);
-
-    toggleNavigation("none");
-});
